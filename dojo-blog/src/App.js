6768a30d888821e8f52bfb7af4ab29303387a2ee
Chase <cnickellsumn@gmail.com>
2023-02-16 23:07:10 -0800

    xcvxcv

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Hey Maeliño'
-  const likes = 50
-
+  
   return (
     <div className="App">
       <Navbar />
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello sluts');
+    }
+
+    const handleClickTwo = (name) => {
+        console.log(`hello ${name}`);
+    }
+
     return ( 
     <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={handleClickTwo}>click me again</button>
     </div> 
     );
 }
