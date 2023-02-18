d5365d7088a1524d95e0d199359a01ac98bf95f3
Chase <cnickellsumn@gmail.com>
2023-02-17 21:33:07 -0800

    Part 12 of tutorial

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,19 +1,18 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-
-    const handleClick = () => {
-        console.log('hello sluts');
-    }
-
-    const handleClickTwo = (name) => {
-        console.log(`hello ${name}`);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     return ( 
-    <div className="home">
-        <h2>Homepage</h2>
-        <button onClick={handleClick}>Click me</button>
-        <button onClick={handleClickTwo}>click me again</button>
-    </div> 
+        <div className="home">
+            <BlogList blogs={ blogs } title="All Blogs!" />
+            <BlogList blogs={ blogs.filter((blogs) => blogs.author === 'mario') } title="Mario's Blogs!" />
+        </div> 
     );
 }
  
