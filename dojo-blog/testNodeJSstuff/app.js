317e794550fbeda8a2af2c5dc811d16934c48581
Chase <cnickellsumn@gmail.com>
2023-04-05 11:00:52 -0700

    learing about html and url requests

--- dojo-blog/testNodeJSstuff/app.js
+++ dojo-blog/testNodeJSstuff/app.js
@@ -1,6 +1,18 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
+const http = require('http')
 
+// about to create a mfin server object
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text'})
+    res.write('<button>ur mom</button>') // THE RESPONSE
+    res.end(); // ends it
+}).listen(6969)
+
+// Register a listener
+emitter.on('messageLogged', () => {
+    console.log('Listener called');
+})
 
 // Raise an event
 emitter.emit('messageLogged');
